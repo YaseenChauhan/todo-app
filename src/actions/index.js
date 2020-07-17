3bad76677c105a3b6c2095de8e823d7b236fde0c
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-18 00:45:28 +0530

    add todo component

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,14 @@ export const addTodo = text => ({
   id: nextTodoId++,
   text
 })
+
+export const toggleTodo = id => ({
+    type: 'TOGGLE_TODO',
+    id
+})
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
