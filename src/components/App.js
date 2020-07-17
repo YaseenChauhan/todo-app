a425ca4d84bea392ac3ea1a54969381c8f8e1866
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-18 00:51:57 +0530

    add todo list component in main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import AddTodo from '../containers/AddTodo'
+import ToDoList from '../containers/ToDoList'
+
 
 const App = () => (
   <div>
     <AddTodo />
+    <ToDoList />
   </div>
 )
 
