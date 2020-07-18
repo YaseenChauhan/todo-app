0d3176cf64f98028b9009bbba17eb98ce4fda911
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-19 02:02:56 +0530

    add icons in todo component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,15 @@
 let id = 0
 
 export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: id++,
-  text
+    type: 'ADD_TODO',
+    id: id++,
+    text
 })
 
-export const deleteTodo = (id) => (
-  {
-      type: 'DELETE_TODO',
-      id
-  }
-)
+export const deleteTodo = id => ({
+    type: 'DELETE_TODO',
+    id
+})
 
 export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Todo = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    style={{
-      textDecoration: completed ? 'line-through' : 'none'
-    }}
-  >
-    {text}
-  </li>
+  <div className="todo">
+    <div
+		onClick={onClick}
+		style={{
+			textDecoration: completed ? 'line-through' : 'none'
+		}}
+		className='todo-item'
+    >
+      	{text}
+    </div>
+    <div>
+		<FontAwesomeIcon icon={faPencilAlt} />
+		<FontAwesomeIcon icon={faTrashAlt} />
+    </div>
+  </div>
 )
 
 Todo.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  completed: PropTypes.bool.isRequired,
-  text: PropTypes.string.isRequired
+	onClick: PropTypes.func.isRequired,
+	completed: PropTypes.bool.isRequired,
+	text: PropTypes.string.isRequired
 }
 
 export default Todo
