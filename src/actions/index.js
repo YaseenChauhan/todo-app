b8fd1c2e22b07713aa44a3e57693cb903b6ef58e
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-18 19:46:52 +0530

    remove filters

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,19 @@
-let nextTodoId = 0
+let id = 0
 
 export const addTodo = text => ({
   type: 'ADD_TODO',
-  id: nextTodoId++,
+  id: id++,
   text
 })
+
+export const deleteTodo = (id) => (
+  {
+      type: 'DELETE_TODO',
+      id
+  }
+)
 
 export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
     id
 })
-
-export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_COMPLETED: 'SHOW_COMPLETED',
-    SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
