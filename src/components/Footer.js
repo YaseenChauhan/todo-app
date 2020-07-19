f3d4c6b1637af5c096d1d40f1d6d7ee42669bc50
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-19 22:37:36 +0530

    typo fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = ({addedTodos}) => {
     return(
         <div>
             <h3>Currently added tasks: {addedTodos} </h3>
-            <div>•Check the box to complete a task.</div>
-            <div>•Click on a task to edit it</div>
+            <div>• Check the box to complete a task.</div>
+            <div>• Click on a task to edit it</div>
         </div>
     )
 }
