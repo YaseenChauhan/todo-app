6c5092727403466f98b91b167266d90e7d424f17
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-19 21:35:31 +0530

    enable / disable button

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
 const AddTodo = ({ dispatch }) => {
-    let input
-
+    let [input, setInput] = useState('')
+    const [disabled, setDisabled] = useState(true)
+    useEffect(() => {
+        if (input) {
+            setDisabled(false)
+        } else {
+            setDisabled(true)
+        }
+    }, [input]);
     return (
         <div className="add-todo-container">
             <form onSubmit={e => {
                 e.preventDefault();
-                dispatch(addTodo(input.value))
-                input.value = ''
+                dispatch(addTodo(input))
+                setInput('')
             }}>
-            <input type="text" ref={value => input = value} className="add-todo-input" placeholder="what to do?" />
-            <button type="submit" className="add-todo">
+            <input 
+                type="text" 
+                onChange={ e => {
+                    setInput(e.target.value)
+                    
+                }} 
+                value={input} 
+                className="add-todo-input" 
+                placeholder="what to do?" 
+            />
+            <button type="submit" className="add-todo" disabled={disabled}>
                 Add Todo
             </button>
             </form>
