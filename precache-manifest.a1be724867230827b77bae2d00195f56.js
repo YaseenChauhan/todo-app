71f4c95ecf0042a7af3143d29b931e20396e0aba
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-19 20:15:34 +0530

    Updates

---
+++ precache-manifest.a1be724867230827b77bae2d00195f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b86298d0e8e43c23114379a6dba959",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "2cd57f5ee368466b410e",
+    "url": "/todo-app/static/css/main.a8992b29.chunk.css"
+  },
+  {
+    "revision": "744e1040817e482ec1aa",
+    "url": "/todo-app/static/js/2.6633a330.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.6633a330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd57f5ee368466b410e",
+    "url": "/todo-app/static/js/main.22f83b4c.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
